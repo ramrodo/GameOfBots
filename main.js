bff348b1014cd094f3c44332b8193acf3b02e84d
Rodolfo Martínez <rodomar@outlook.com>
2017-05-28 14:33:44 -0500

    Final main

--- main.js
+++ main.js
@@ -1,69 +1,43 @@
-exports.handler = (event, context, callback) => {
-  // process GET request
-  if (event.params && event.params.querystring) {
-    var queryParams = event.params.querystring;
- 
-    var rVerifyToken = queryParams['hub.verify_token'];
- 
-    if (rVerifyToken === VERIFY_TOKEN) {
-      var challenge = queryParams['hub.challenge'];
-      callback(null, parseInt(challenge));
-    } else {
-      callback(null, 'Error, wrong validation token');
-    }
- 
-  // process POST request
-  } else {
-    var messagingEvents = event.entry[0].messaging;
-    for (var i = 0; i < messagingEvents.length; i++) {
-      var messagingEvent = messagingEvents[i];
- 
-      console.log("Received a message event:", JSON.stringify(messagingEvent, null, 2));
-      if (messagingEvent.message && !messagingEvent.message.is_echo) {
-        var sender = messagingEvent.sender.id;
-        getConversation(sender).then(function(data) {
-          console.log("Stored conversation:", JSON.stringify(data, null, 2));
-            
-          var conversation = data.Item;
-          // Is known?
-          if (conversation) {
-            // Is known: yes
-            // If message contains text echo it, otherwise send a thumbs up
-            var messageText = "\ud83d\udc4d";
-            if (messagingEvent.message.text) {
-              conversation.lastMessage = messagingEvent.message.text;
-              messageText = "Text received, echo: " +
-                messagingEvent.message.text.substring(0, 200);
-            }
-            
-            return {
-              conversation: conversation,
-              messageData: createTextMessageData(sender, messageText),
-              updateDB: conversation.hasJoined
-            };
-          } else {
-            // Is known: no
-            return processJoinMessage(messagingEvent.message, sender);
-          }
-        }).then(function(result) {
-        if (result.messageData) {
-            callSendAPI(result.messageData);
-        }
-          
-          return result;
-      }).then(function(result) {
-          if (result.updateDB) {
-          console.log("There is an update to store:", JSON.stringify(result.conversation, null, 2));
-          return putConversation(result.conversation);
-        }
-      }).catch(function(error) {
-        console.error("Error:", JSON.stringify(error, null, 2));
-        });
+/*jshint esversion: 6 */
+
+export default class Bot {
+
+   constructor(args){
+     this.args = args;
+   }
+
+   execute(){
+     let args = this.args;
+     let text = args.text;
+
+     return new Promise ((resolve)=>{
+
+       if (text){
+
         
-        callback(null, "Done");
-      }
-    }
- 
-    callback(null, event);
+          switch (text) {
+            case '1':
+              resolve({"text": "Canción 1 elegida"});
+              break;
+            case '2':
+              resolve({"text": "Canción 2 elegida"});
+              break;
+            case '3':
+              resolve({"text": "Canción 3 elegida"});
+              break;
+            case '4':
+              resolve({"text": "Canción 4 elegida"});
+              break;
+            case '5':
+              resolve({"text": "Canción 5 elegida"});
+              break;
+            default:
+              resolve({"text": "No entiendo"});
+              break;
+          }
+
+       }
+     });
+
+   }
   }
-};
